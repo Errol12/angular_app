e6290c56d56bfda2e0b2442225a8a8bbd30f0d5c
Errol <errolf07@gmail.com>
2018-08-12 13:04:13 +0530

    First Commit
    Signed-off-by: Errol  <errolf07@gmail.com>

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `<p> Hello world {{name}} 
+  				{{2+1}}  
+  				{{"hello" + name}}
+  				{{name.length}}
+  				{{name.toUpperCase()}}
+  				{{greetUser()}}
+  				{{siteUrl}}
+  			</p>
+  			<br>
+  			<input [id]="myId" type="text" [class]="success" value="george" name="inp"/> 
+  			<input id="{{myId}}" type="text" class="text-special" value="george" name="inp"/>
+  			<br>
+  			<h2 [ngClass]="messageClasses">CodeEvolution</h2>
+  			<button (click)="onClick()">Greet</button>
+  			<button (click)="onClick1($event)">Event Greet</button>
+  			<button (click)="name='Robert'">Event Greet</button>
+  			`,
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  public name = "Mark";
+  public myId="test_id";
+  public success = "text-success";
+  public hasError = false;
+  public isSpecial = true;
+  public messageClasses = {
+  	"text-success": !this.hasError,
+  	"text-failure": this.hasError,
+  	"text-special":this.isSpecial
+  }
+  public siteUrl = window.location.href;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  greetUser(){
+  	return "hello" + this.name;
+  }
+
+  onClick(){
+  	console.log('triggered');
+  	this.name = "Errol";
+  }
+
+  onClick1(event){
+  	console.log(event);
+  	this.name = "Errol";
+  }
+
+}
